3c766a8d1b53769826e4b1bf162c88579b817fdb
Jian Furquim <furquimjian@gmail.com>
2024-03-11 22:32:37 -0300

    Initial viewset for User app

---
+++ apps/users/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+
+from apps.users.models import User
+from apps.users.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'username'
