a7463c003865b9fd9fa24cce28180516bb4aad86
Jian Furquim <furquimjian@gmail.com>
2024-03-14 10:26:46 -0300

    Add frontend app in Project

--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "apps.user",
     "apps.loan",
     "apps.payment",
+    "apps.frontend",
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -25,7 +25,8 @@ router = DefaultRouter()
 router.register(r"user", UserViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("admin/", admin.site.urls),
+    path("", include("apps.frontend.urls")),
 ]
