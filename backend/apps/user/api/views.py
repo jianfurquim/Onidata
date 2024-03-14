f78e95872d3388a6befd15979897f044244e04ed
Jian Furquim <furquimjian@gmail.com>
2024-03-14 18:37:08 -0300

    Add in backend folder the Project

--- backend/apps/user/api/views.py
+++ backend/apps/user/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from apps.user.api.serializers import UserSerializer
 
@@ -8,3 +8,4 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = "username"
+    permission_classes = [IsAuthenticated]
