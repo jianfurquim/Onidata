3265feacdc68be4171f1d4b4a533a654057ecc63
Jian Furquim <furquimjian@gmail.com>
2024-03-14 17:08:34 -0300

    Add field amount of payments in Loan model

--- apps/loan/models.py
+++ apps/loan/models.py
@@ -10,6 +10,9 @@ class Loan(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     is_active = models.BooleanField(_("Active"), default=True)
     value = models.DecimalField(_("Nominal Value"), max_digits=13, decimal_places=3)
+    amount_of_payments = models.PositiveIntegerField(
+        _("Amount of Payments"), default=12
+    )
     interest_rate = models.DecimalField(
         _("Interest Rate"), max_digits=6, decimal_places=3
     )
