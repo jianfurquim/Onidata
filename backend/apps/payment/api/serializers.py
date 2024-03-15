31148d04eaf13e2952b88a366e93c88c304a91ae
Jian Furquim <furquimjian@gmail.com>
2024-03-15 17:45:34 -0300

    Changes to the Payment serializer

--- backend/apps/payment/api/serializers.py
+++ backend/apps/payment/api/serializers.py
@@ -3,6 +3,22 @@ from apps.payment.models import Payment
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+
+    client = serializers.ReadOnlyField(source="loan.client")
+    bank = serializers.ReadOnlyField(source="loan.bank")
+
     class Meta:
         model = Payment
-        fields = "__all__"
+        fields = [
+            "pk",
+            "loan",
+            "client",
+            "bank",
+            "value",
+            "total_value",
+            "interest_value",
+            "date",
+            "due_date",
+            "effective_date",
+            "is_paid",
+        ]
